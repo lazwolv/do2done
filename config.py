fec103218b11f9b0be6fc4dcbce801c312840ce3
Laz <lazcodes@gmail.com>
2024-12-29 16:52:39 -0800

    Reset git tracking according to new gitignore rules

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:postgres@localhost:5432/do2done'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    f"postgresql://postgres:{os.environ.get('DB_PASSWORD')}@localhost:5432/do2done"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
     TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
